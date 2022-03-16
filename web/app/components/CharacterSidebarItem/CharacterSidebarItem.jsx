2b2e38611f4fe5a498af4037163fb67dadd7c360
raymo <raymondnguyen12@outlook.com>
2022-03-16 16:19:54 -0500

    wip/saving-changes

---
+++ web/app/components/CharacterSidebarItem/CharacterSidebarItem.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const CharacterSidebarItem = () => {
+  return <div>CharacterSidebarItem</div>;
+};
--- web/app/routes/characters/$slug.jsx
+++ web/app/routes/characters/$slug.jsx
@@ -1,8 +1,49 @@
 // remix
-import React from "react";
+import { useLoaderData } from "remix";
+import client from "~/lib/sanity/client";
+// third party
+import groq from "groq";
+// styles
+import styles from "../../components/CharacterSidebarItem/CharacterSidebarItem.css";
+
+export function links() {
+  return [
+    {
+      rel: "stylesheet",
+      href: styles,
+    },
+  ];
+}
+
+export const loader = async (context) => {
+  const slug = context.params.slug;
+
+  const character = await client.fetch(
+    groq`*[_type == "characters" && slug.current == $slug]`,
+    { slug }
+  );
+
+  return character;
+};
 
 export const Character = () => {
-  return <div>Character</div>;
+  const data = useLoaderData();
+
+  console.log(data[0]);
+
+  return (
+    <ul className="character_sidebar">
+      {Object.keys(data[0]).map((key) => {
+        const value = data[0][key];
+
+        return (
+          <li key={value._id}>
+            <h1>{key}</h1>
+          </li>
+        );
+      })}
+    </ul>
+  );
 };
 
 export default Character;
