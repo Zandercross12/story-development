1b37c7f3f28734ee657b1e62346b781c0bf58ebc
Raymond Nguyen <rn@buildmidwestern.com>
2022-04-04 16:00:56 -0500

    wip/saving-changes

--- web/app/components/Carousel/Carousel.jsx
+++ web/app/components/Carousel/Carousel.jsx
@@ -8,25 +8,32 @@ const builder = imageUrlBuilder(client);
 
 export const Carousel = ({ data }) => {
   return (
-    <ul className="carousel_list">
-      {data.map((item, index) => {
-        return (
-          <li className="carousel_list_item" key={index}>
-            <Link to="#">
-              <div className="carousel_image_container">
-                {!item.image ? (
-                  <img src="/file-solid.svg" alt="carouselImage" />
-                ) : (
-                  <img src={builder.image(item?.image)} alt="carouselImage" />
-                )}
-              </div>
-              <h2>{item?.name}</h2>
-              <p className="carousel_card_description">{item?.description}</p>
-            </Link>
-          </li>
-        );
-      })}
-    </ul>
+    <>
+      <ul className="carousel_list">
+        {data.map((item, index) => {
+          return (
+            <li className="carousel_list_item" key={index}>
+              <Link to="#">
+                <div className="carousel_image_container">
+                  {!item.image ? (
+                    <img src="/file-solid.svg" alt="carouselImage" />
+                  ) : (
+                    <img src={builder.image(item?.image)} alt="carouselImage" />
+                  )}
+                </div>
+                <h2>{item?.name}</h2>
+                <p className="carousel_card_description">{item?.description}</p>
+                <div className="carousel_item_read_more">Read More</div>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+      <div className="carousel_arrows">
+        <button>left</button>
+        <button>right</button>
+      </div>
+    </>
   );
 };
 
