2b8666241b42c2783125633e65562b64847fe298
Raymond Nguyen <rn@buildmidwestern.com>
2022-04-11 16:00:33 -0500

    [feat] Updated Magic Abilities

--- web/app/routes/magic/abilities/$ability/index.jsx
+++ web/app/routes/magic/abilities/$ability/index.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+// react | remix
+import { useLoaderData } from "remix";
+// sanity client
+import client from "~/lib/sanity/client";
+// third-party
+import groq from "groq";
+import imageUrlBuilder from "@sanity/image-url";
+// components
+// styles
 
 export const Ability = () => {
-  return <div>Ability</div>;
+  return (
+    <>
+      <section>
+        <div className="container">
+          <h1>Magic Name</h1>
+          <p>Magic Description</p>
+        </div>
+      </section>
+      <section>
+        <div className="container">
+          <h1>Action</h1>
+          <p></p>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Ability;
