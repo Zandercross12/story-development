ef755c7017997f7941b614a1e884f2d9395ac1a2
Raymond Nguyen <rn@buildmidwestern.com>
2022-04-26 15:51:31 -0500

    [feat] updated timeline

--- web/app/components/Timeline/Timeline.jsx
+++ web/app/components/Timeline/Timeline.jsx
@@ -2,7 +2,7 @@
 import React, { useState } from "react";
 
 export const Timeline = ({ timeline, events, name }) => {
-  console.log(timeline, "\n\n\n", events);
+  const [preview, setPreview] = useState(undefined);
 
   const next = () => {};
 
@@ -10,15 +10,51 @@ export const Timeline = ({ timeline, events, name }) => {
 
   return (
     <>
+      <div className="timeline_preview_container">
+        <div className="timeline_preview">
+          {preview ? (
+            <>
+              <h1>{preview?.name}</h1>
+              <h5>
+                {preview?.startDate && preview?.endDate ? (
+                  <>
+                    {preview?.startDate} - {preview?.endDate}
+                  </>
+                ) : (
+                  <>{preview?.startDate || preview?.endDate}</>
+                )}
+              </h5>
+              <br />
+              <p>{preview?.description}</p>
+            </>
+          ) : (
+            <h1>Select a timeline dot</h1>
+          )}
+        </div>
+      </div>
       <ul className="timeline_list">
         {timeline?.events.map((item, index) => {
+          const itemEvent = events.find((event) => event?._id === item?._ref);
+          console.log(itemEvent);
           return (
             <li className={`timeline_list_item ${name}`} key={index}>
-              {index === 0 && <i class="fa-solid fa-minus"></i>}
-              <button>
-                <i class="fa-solid fa-circle-dot"></i>
+              {index === 0 && <i className="fa-solid fa-minus"></i>}
+              <button
+                onClick={(target) => {
+                  const selectedButtons = document.getElementsByClassName(
+                    "timeline_button_selected"
+                  );
+                  Array.from(selectedButtons).forEach((button) => {
+                    button.classList.remove("timeline_button_selected");
+                  });
+                  target.target.classList.add("timeline_button_selected");
+                  setPreview(itemEvent);
+                }}
+                title={itemEvent?.name}
+              >
+                <i className="fa-solid fa-circle-dot"></i>
               </button>
-              <i class="fa-solid fa-minus"></i>
+              <i className="fa-solid fa-minus"></i>
             </li>
           );
         })}
